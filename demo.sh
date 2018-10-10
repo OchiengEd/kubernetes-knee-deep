f92a0d1dd4934d72a91f276de0e19069714ffc6f
Edmund Ochieng <ochienged@gmail.com>
2018-10-09 19:55:29 -0500

    Added checking resources after deployment

--- demo.sh
+++ demo.sh
@@ -8,7 +8,8 @@ deploy_trivia_webapp() {
   scriptreplay --timing assets/deploy.time assets/deploy.screen
 }
 
-verify_pods_running() {
+verify_deployment() {
+  scriptreplay --timing assets/deploycheck.time assets/deploycheck.screen
 }
 
 echo "# Ensure there are no applications running in the default namespace" | pv -qL 14
@@ -18,4 +19,4 @@ resource_check
 deploy_trivia_webapp
 
 echo "# Verify all pods are up and running" | pv -qL 14
-verify_pods_running
+verify_deployment
